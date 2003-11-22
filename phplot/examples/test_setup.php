f09c4f4352c65900e7dcd29531f25200f555096d
migueldb <migueldb@users.sf.net>
2003-11-22 17:04:17 +0000

    + Added wbmp format + Now using imagetypes() instead of function_exists()
    git-svn-id: https://svn.code.sf.net/p/phplot/code/trunk@59 1589d9c6-5bb9-42a2-b2f8-3691a6793a8d

--- phplot/examples/test_setup.php
+++ phplot/examples/test_setup.php
@@ -1,8 +1,8 @@
 <!DOCTYPE HTML PUBLIC "-//W3C//DTD HTML 4.0 Transitional//EN">
-<!-- $Id -->
+<!-- $Id$ -->
 <html>
 <head>
-   <title>PHPlot Quickstart</title>
+   <title>PHPlot graphic formats test</title>
    <style type="text/css">
     <!--
     td { text-align:center; }
@@ -12,11 +12,11 @@
 </head>
 <body>
 
-<h2>PHPlot Quickstart</h2>
+<h2>PHPlot graphic formats test</h2>
 
 <p>
-This page will test if you have GD and PHP set up to view images correctly. 
-You should see at least one of the three images below. 
+This page will test which graphic formats are supported by the version of GD 
+linked into PHP. You should see at least one of the four images below. 
 </p>
 
 <center>
@@ -24,7 +24,7 @@ You should see at least one of the three images below.
 <tr><td class="hdr">PNG graphics</td></tr>
 <tr><td>
 <?php
-    if (! function_exists("imagepng"))  
+    if (! imagetypes() & IMG_PNG )  
     	echo "PNG NOT ENABLED";
     else
 	    echo "<img src=\"inline_image.php?file_format=png&which_title=YES_PNG_IS_ENABLED\" />";
@@ -34,7 +34,7 @@ You should see at least one of the three images below.
 <tr><td class="hdr">JPEG graphics</td></tr>
 <tr><td>
 <?php
-    if (! function_exists("imagejpeg"))
+    if (! imagetypes() & IMG_JPG )
         echo "JPEG NOT ENABLED";
     else
 	    echo "<img src=\"inline_image.php?file_format=jpg&which_title=YES_JPG_IS_ENABLED\" />";
@@ -43,10 +43,19 @@ You should see at least one of the three images below.
 <tr><td class="hdr">GIF graphics</td></tr>
 <tr><td>
 <?php
-    if (! function_exists("imagegif"))
+    if (! imagetypes() & IMG_GIF)
 	    echo "GIF NOT ENABLED";
     else
 	    echo "<img src=\"inline_image.php?file_format=gif&which_title=YES_GIF_IS_ENABLED\" />";
+?>
+</td></tr>
+<tr><td class="hdr">BMP graphics</td></tr>
+<tr><td>
+<?php
+    if (! imagetypes() & IMG_WBMP)
+	    echo "BMP NOT ENABLED";
+    else
+	    echo "<img src=\"inline_image.php?file_format=wbmp&which_title=YES_WBMP_IS_ENABLED\" />";
 ?>
 </td></tr>
 </table>
