55039f47fc81ccb8bea0d479de49a5520efddee5
migueldb <migueldb@users.sf.net>
2003-11-22 12:57:25 +0000

    + Graphic formats availability checks now use function_exists() + HTML code restructured.
    git-svn-id: https://svn.code.sf.net/p/phplot/code/trunk@58 1589d9c6-5bb9-42a2-b2f8-3691a6793a8d

--- phplot/examples/test_setup.php
+++ phplot/examples/test_setup.php
@@ -1,80 +1,56 @@
 <!DOCTYPE HTML PUBLIC "-//W3C//DTD HTML 4.0 Transitional//EN">
-<HTML><HEAD>
-<TITLE>PHPlot Quickstart</TITLE>
-</HEAD>
-<BODY>
+<!-- $Id -->
+<html>
+<head>
+   <title>PHPlot Quickstart</title>
+   <style type="text/css">
+    <!--
+    td { text-align:center; }
+    .hdr { font-weight: bold; }
+    -->
+   </style>
+</head>
+<body>
 
 <h2>PHPlot Quickstart</h2>
 
-This page will test to see if you have GD and PHP set up to 
-view images correctly. You should see at least one
-of the three images below. 
 <p>
-<?php
-/* *************************
-error_reporting(4);
-$im_test = ImageCreate(2,2);
-$test = (ImagePng($im_test)) ;
-ImageDestroy($im_test);
-exit;
-**************************** */
-?>
+This page will test if you have GD and PHP set up to view images correctly. 
+You should see at least one of the three images below. 
+</p>
 
-<p>
-<TABLE border=1>
-<TR><TD>Test to see if PNG graphs work</TD></TR>
-<TR><TD>
+<center>
+<table>
+<tr><td class="hdr">PNG graphics</td></tr>
+<tr><td>
 <?php
-$im_test = ImageCreate(1,1);
-echo "<!--";
-if (! ImagePng($im_test) ) { 
-	//In php3 this is a fatal error, in php4 it is a warning. 
-	echo "-->";
-	echo "PNG NOT ENABLED";
-} else { 
-	echo "-->";
-	?>
-	<IMG SRC="inline_image.php?file_format=png&which_title=YES_PNG_IS_ENABLED"></TD></TR>
-	<?php
-}
-ImageDestroy($im_test);
+    if (! function_exists("imagepng"))  
+    	echo "PNG NOT ENABLED";
+    else
+	    echo "<img src=\"inline_image.php?file_format=png&which_title=YES_PNG_IS_ENABLED\" />";
+    
 ?>
-</TABLE>
-<p>
-<TABLE border=1>
-<TR><TD>Test to see if JPEG graphs work</TD></TR>
-<TR><TD>
+</td></tr>
+<tr><td class="hdr">JPEG graphics</td></tr>
+<tr><td>
 <?php
-$im_test = ImageCreate(1,1);
-if (! ImageJPEG($im_test) ) { 
-	echo "JPEG NOT ENABLED";
-} else { 
-	?>
-	<IMG SRC="inline_image.php?file_format=jpg&which_title=YES_JPG_IS_ENABLED"></TD></TR>
-	<?php
-}
-ImageDestroy($im_test);
+    if (! function_exists("imagejpeg"))
+        echo "JPEG NOT ENABLED";
+    else
+	    echo "<img src=\"inline_image.php?file_format=jpg&which_title=YES_JPG_IS_ENABLED\" />";
 ?>
-</TABLE>
-
-<TABLE border=1>
-<TR><TD>Test to see if GIF graphs work</TD></TR>
-<TR><TD>
+</td></tr>
+<tr><td class="hdr">GIF graphics</td></tr>
+<tr><td>
 <?php
-$im_test = ImageCreate(1,1);
-echo "<!--";
-if (! ImageGif($im_test)) { 
-	echo "-->";
-	echo "GIF NOT ENABLED<br>";
-} else { 
-	echo "-->";
-	?>
-	<IMG SRC="inline_image.php?file_format=gif&which_title=YES_GIF_IS_ENABLED"></TD></TR>
-	<?php
-}
-ImageDestroy($im_test);
+    if (! function_exists("imagegif"))
+	    echo "GIF NOT ENABLED";
+    else
+	    echo "<img src=\"inline_image.php?file_format=gif&which_title=YES_GIF_IS_ENABLED\" />";
 ?>
-</TABLE>
+</td></tr>
+</table>
+</center>
 
 </body>
 </html>
