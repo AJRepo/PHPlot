dbe759adaee79b409a8ebb475a7105e074c8663a
migueldb <migueldb@users.sf.net>
2003-11-22 12:55:57 +0000

    + Using $_GET and $_POST + SetImageFormat() option added
    git-svn-id: https://svn.code.sf.net/p/phplot/code/trunk@57 1589d9c6-5bb9-42a2-b2f8-3691a6793a8d

--- phplot/examples/create_chart.php
+++ phplot/examples/create_chart.php
@@ -1,4 +1,5 @@
 <?php    
+/* $Id$ */
 
 extract ($_GET, EXTR_OVERWRITE);
 extract ($_POST, EXTR_OVERWRITE);
@@ -29,29 +30,26 @@ if ($which_data_type =="function") {
 
 //Required Settings 
 	include("../phplot.php");
-	$graph = new PHPlot($XSIZE_in, $YSIZE_in);
+	$graph = new PHPlot($xsize_in, $ysize_in);
 	$graph->SetDataType($which_data_type);  //Must be first thing
 
-	//$graph->SetFileFormat("png");
 	$graph->SetDataValues($data);
 
 //Optional Settings (Don't need them) 
+	$graph->SetFileFormat($which_file_format);
 	$graph->SetPlotType($which_plot_type);
 	$graph->SetUseTTF("0");
 	$graph->SetVertTickIncrement($which_vti);
 	$graph->SetHorizTickIncrement($which_hti);
     $graph->SetLineWidth("1");
 	$graph->SetDrawYGrid("1"); // 1 = true
-	$graph->SetPointShape("$which_dot");
-	$graph->SetErrorBarShape("$which_error_type");
+	$graph->SetPointShape($which_dot);
+	$graph->SetErrorBarShape($which_error_type);
 	$graph->SetXLabel($xlbl);
 	$graph->SetYLabel($ylbl);
 	$graph->SetTitle($title);
 	$graph->SetXAxisPosition($which_xap);
-	$graph->SetDataColors(
-		array("blue","green","yellow","red"),  //Data Colors
-		array("black")							//Border Colors
-	);  
+    
 	if ($maxy_in) { 
 		if ($which_data_type = "text-linear") { 
 			$graph->SetPlotAreaWorld(0,$miny_in,count($data),$maxy_in);
@@ -67,14 +65,19 @@ if ($which_data_type =="function") {
 
 /*
 //Even more settings
-		$graph->SetPlotBgColor(array(222,222,222));
-		$graph->SetBackgroundColor(array(200,222,222)); //can use rgb values or "name" values
-		$graph->SetTextColor("black");
-		$graph->SetGridColor("black");
-		$graph->SetLightGridColor(array(175,175,175));
-		$graph->SetTickColor("black");
-		$graph->SetTitleColor(array(0,0,0)); // Can be array or name
+        $graph->SetDataColors(
+	        array("blue","green","yellow","red"),  //Data Colors
+            array("black")							//Border Colors
+        );  
+
+        $graph->SetPlotBgColor(array(222,222,222));
+        $graph->SetBackgroundColor(array(200,222,222)); //can use rgb values or "name" values
+        $graph->SetTextColor("black");
+        $graph->SetGridColor("black");
+        $graph->SetLightGridColor(array(175,175,175));
+        $graph->SetTickColor("black");
+        $graph->SetTitleColor(array(0,0,0)); // Can be array or name
 */
 
-    $graph->DrawGraph();
+      $graph->DrawGraph();
 ?>
