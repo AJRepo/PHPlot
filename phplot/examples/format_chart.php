c199e0b8211aefca610d79bb8a532411aa37dc18
migueldb <migueldb@users.sf.net>
2003-12-07 00:04:16 +0000

    + Added a check to see if we are being called from the right place. + New data label angle option. + New line and error bar line width options. + New data label options. + New point types. + Some corrections.
    git-svn-id: https://svn.code.sf.net/p/phplot/code/trunk@107 1589d9c6-5bb9-42a2-b2f8-3691a6793a8d

--- phplot/examples/format_chart.php
+++ phplot/examples/format_chart.php
@@ -1,12 +1,15 @@
 <!DOCTYPE HTML PUBLIC "-//W3C//DTD HTML 4.0 Transitional//EN">
 <html>
 <head>
-    <title>Test Graph creation</title>
+    <title>Example-o-matic</title>
     <link type="text/css" rel="stylesheet" href="../doc/style.css" />
 </head>
 <body>
 
+<div class="nav"> [ <a href="../doc/index.html">Go to the index</a> ] </div>
+
 <h2>PHPlot test graph form</h2>
+
 <p>Use this form to test many different options of PHPlot. You can test
 every graph type supported for any of four different data types. You can
 tweak as you like or you can leave everything as is and press "Submit" for
@@ -34,7 +37,7 @@ automatic values.
   ?>
   </td>
 </tr>
-        <tr><td colspan="2" class="hdr"><input type="submit" /></td></tr>
+        <tr><td colspan="2" class="hdr"><input name="submit" type="submit" /></td></tr>
         <tr>
           <td colspan="2">
             <br />
@@ -46,19 +49,19 @@ automatic values.
         
 <tr>
   <td>Width of graph in pixels:</td>
-  <td><input type="text" name="xsize_in" value="600" /></td>
+  <td><input type="text" name="xsize_in" value="600" size="4" /></td>
 </tr>
 <tr>
   <td> Height of graph in pixels:</td>
-  <td><input type="text" name="ysize_in" value="400" /></td>
+  <td><input type="text" name="ysize_in" value="400" size="4" /></td>
 </tr>
 <tr>  
   <td>Maximum height of graph in y axis units:</td>
-  <td><input type="text" name="maxy_in" value="" /></td>
+  <td><input type="text" name="maxy_in" value="" size="4" /></td>
 </tr>
 <tr>
   <td>Minimum height of graph in y axis units:</td>
-  <td><input type="text" name="miny_in" value="" /></td>
+  <td><input type="text" name="miny_in" value="" size="4" /></td>
 </tr>
 
             <tr><td colspan="2" class="hdr">Titles and labels</td></tr>
@@ -83,16 +86,34 @@ automatic values.
   </td>
 </tr>
 <tr>
-  <td>Y axis data labels position:</td>
+  <td>Y axis tick labels position:</td>
   <td>
-    <select name="which_ylabel_pos">
+    <select name="which_ytick_label_pos">
       <option value="plotleft">Left of plot</option>
       <option value="plotright">Right of plot</option>
       <option value="both" selected>Both right and left</option>
-      <option value="none">No labels</option>
+      <option value="none">No tick labels</option>
     </select>
   </td>
 </tr>
+<tr>
+  <td>Y axis data labels position:</td>
+  <td>
+    <select name="which_ydata_label_pos">
+      <option value="plotleft">Left of plot</option>
+      <option value="plotright">Right of plot</option>
+      <option value="both">Both right and left</option>
+      <option value="plotin">In the plot (To Do)</option>
+      <option value="none" selected>No data labels</option>
+    </select>
+  </td>
+</tr>
+<tr>
+  <td>Y axis labels angle:</td>
+  <td><input name="which_ylabel_angle" value="0" size="3" /></td>
+</tr>
+
+
 <tr>
   <td>X axis title:</td>
   <td><input type="text" name="xlbl" value="years" /></td>
@@ -109,15 +130,31 @@ automatic values.
   </td>
 </tr>
 <tr>
-  <td>X axis data labels position:</td>
+  <td>X axis tick labels position:</td>
   <td>
-    <select name="which_xlabel_pos">
+    <select name="which_xtick_label_pos">
       <option value="plotup">Up of plot</option>
       <option value="plotdown">Down of plot</option>
       <option value="both" selected>Both up and down</option>
-      <option value="none">No labels</option>
+      <option value="none">No tick labels</option>
     </select>
   </td>
+</tr>
+<tr>
+  <td>X axis data labels position:</td>
+  <td>
+    <select name="which_xdata_label_pos">
+      <option value="plotup">Up of plot</option>
+      <option value="plotdown">Down of plot</option>
+      <option value="both" selected>Both up and down</option>
+      <option value="plotin">In the plot (To Do)</option>
+      <option value="none" selected>No data labels</option>
+    </select>
+  </td>
+</tr>
+<tr>
+  <td>X axis labels angle:</td>
+  <td><input name="which_xlabel_angle" value="0" size="3" /></td>
 </tr>
 
             <tr><td colspan="2" class="hdr">Grid and ticks</td></tr>
@@ -188,6 +225,14 @@ automatic values.
   <td><input type="text" name="which_shading" value="5" size="3"/></td>
 </tr>  
 <tr>
+  <td>Plot line width:</td>
+  <td><input name="which_line_width" value="1" size="3" /></td>
+</tr>
+<tr>
+  <td>Error bar line width:</td>
+  <td><input name="which_errorbar_line_width" value="1" size="3" /></td>
+</tr>
+<tr>
   <td>Point Type:</td>
   <td>
     <select name="which_point">
@@ -195,10 +240,12 @@ automatic values.
   	  <option value="rect">Square</option>
 	  <option value="circle">Circle</option>
 	  <option value="triangle">Triangle</option>
+	  <option value="trianglemid">Centered triangle</option>
       <option value="dot">Filled dot</option>
 	  <option value="line">Line</option>
 	  <option value="halfline">Half line</option>
-      <option value="crosshair" selected>Crosshair</option>
+      <option value="cross" selected>Cross</option>
+      <option value="plus" selected>Plus sign</option>
     </select>
   </td>
 </tr>
@@ -226,7 +273,7 @@ automatic values.
     </select>
   </td>
 </tr>
-        <tr><td colspan="2" class="hdr"><input type="submit" /></td></tr>
+        <tr><td colspan="2" class="hdr"><input name="submit" type="submit" /></td></tr>
 
 </table>
 </center>
@@ -238,8 +285,6 @@ Please visit <a href="http://phplot.sourceforge.net">PHPlot's site</a>, the
 or see <a href="http://www.jeo.net/php/">more php code and examples</a> 
 by Afan Ottenheimer of jeonet.
 </p>
-
-<p><a href="../doc/index.html">Click here</a> to return to the documentation index.</p>
 
 <p class="foot">$Id$</p>
 </body>
