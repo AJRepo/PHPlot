f662f9e0e7d17ba85b0c4715f9cb26b3ce4ff67f
migueldb <migueldb@users.sf.net>
2003-12-27 12:46:03 +0000

    + Added all the examples. + Added 5.0 version number. + Added myself as author.
    git-svn-id: https://svn.code.sf.net/p/phplot/code/trunk@137 1589d9c6-5bb9-42a2-b2f8-3691a6793a8d

--- phplot/doc/index.php
+++ phplot/doc/index.php
@@ -7,7 +7,7 @@
 </head>
 <body>
 
-<h2>Welcome to PHPlot</h2>
+<h2>Welcome to PHPlot 5.0</h2>
 <table border="0">
  <tr>
   <td valign="top">
@@ -15,17 +15,18 @@
    PHPlot is a <a href="http://www.php.net">PHP4</a> class for on the fly graphs
    generation. It was started by
    <a href="mailto:afan AT jeo DOT net">Afan Ottenheimer</a> in 2000 as an
-   opensource project, and is now co-developed thanks to 
+   opensource project, and is now co-developed with 
+   <a href="mailto:nonick AT 8027 DOT org">Miguel de Benito</a> thanks to 
    <a href="http://sourceforge.net">sourceforge</a>. It is distributed under
    the terms of the 
    <a href="http://www.gnu.org/copyleft/gpl.html"> GNU General Public License</a>,
    and the <a href="http://www.php.net/license.html">PHP license</a>. You can always
    obtain the latest source from the <a href="http://sourceforge.net/projects/phplot/">
-  sourceforge project page</a>, please check CVS, we try to have it always working
+  sourceforge project page</a>, please do also check CVS, we try to have it always working
   there.
   </p>
-  <p>For further information, please check <a href="http://www.jeo.net/php/phplot/">
-  Afan's site</a>
+  <p>For further information, please check <a href="http://www.phplot.com">
+  our website</a>
   </p>
   </td>
   <td>
@@ -50,22 +51,23 @@
    <p>
    Here goes a (incomplete) list, in no particular order.:
    <ul>
-    <li>Several different graph types: lines, bars, points, areas, pie. </li>
+    <li>Several different graph types: lines, bars, points, areas, pie, squared.</li>
+    <li>text-data, data only and data-error data types accepted.</li>
     <li>3D shading for pie and bar graphs.</li>
-    <li>Different line types, customizable. </li>
+    <li>Different line types: solid and wholly customizable dashed ones.</li>
     <li>Can draw error margins along y-axis when supplied in data. </li>
     <li>Highly customizable canvas: titles, labels and ticks can be 
-        placed anywhere, with any color.</li>
-    <li>Grids.</li>
+        placed anywhere, with any color and everything gets automagically placed without overlapping.</li>
+    <li>Vertical and horizontal grids.</li>
     <li>Legend. Different types on the works. </li>
-    <li>TrueType font support. </li>
+    <li>TrueType font support.</li>
     <li>Linear and logaritmic scales.</li>
     <li>Several output formats: jpeg, png, gif, wbmp (those supported by your GD)</li>
    </ul>
    And here a short whishlist:
    <ul>
-    <li>(fillme)</li>
-    <li>(fillme)</li>
+    <li>Horizontal bars</li>
+    <li>Simple isometric 3D plots</li>
    </ul>
    </p>
   </td>
@@ -75,8 +77,8 @@
 
 <h3>Requirements</h3>
 <p>
-We are not sure about exact requirements, but at least PHP 4.0.6 and 
-GD Lib 2 are necessary.
+We are not sure about exact requirements, but at least PHP 4.1.0 and 
+GD Lib 2 are necessary. Feedback is welcome.
 </p>
 
 
@@ -86,18 +88,24 @@ These examples make use of many, but not all, of the features present in PHPlot.
 The best one is the example-o-matic, where you can alter many parameters. Please proceed
 to any of them:
 <ul>
-    <li><a href="../examples/test_setup.php">Setup test</a></li>
+    <li><a href="../examples/test_setup.php">GD setup test</a></li>
     <li>Examples:</li>
         <ul>
-            <li><a href="../examples/format_chart.php">Example-o-matic</a>, 
-            needs some user interaction, though</li>
-        <li><a href="../examples/example1.php">Chart</a></li>
-        <li><a href="../examples/example4.php">Stock Chart</a></li>
+          <li><a href="../examples/format_chart.php">Example-o-matic</a>. 
+            Create most of the plot types with all data types and tweak most of the parameters.</li>
+          <li><a href="../examples/example1.php">Simple lines chart</a></li>
+          <li><a href="../examples/example2.php">Another one</a></li>
+          <li><a href="../examples/example3.php">With labels</a></li>
+          <li><a href="../examples/example4.php">Stock Chart (log scale)</a></li>
+          <li><a href="../examples/example6.php">Thin bar lines</a></li>
+          <li><a href="../examples/example7.php">Log chart with errors</a></li>
+          <li><a href="../examples/example8.php">Two plots in one image</a></li>
+          <li><a href="../examples/example9.php">Chart with some lines in it</a></li>
         </ul>
     <li><b>NOTE:</b>If the examples don't seem to work for you, it may be that you
-        don't have PHP set up correctly.If you do you should see some bold text here:
+        don't have PHP set up correctly. If you do you should see some bold text here:
         <?php echo "<b>OK!</b>\n"; ?> <br />
-        If you see no link then you should contact your system
+        If you see no text then you should contact your system
         administrator or your webserver documentation as to how to configure this.
     </li>
 </ul>
@@ -108,7 +116,7 @@ to any of them:
 Description of the use and inner workings of PHPlot:
 <ul>
     <li><a href="schema.html">PHPlot canvas' elements drawing</a></li>
-    <li>Function reference: (<b>Very</b> outdated)
+    <li>Function reference: (<b>Very outdated</b>)
         <ul>
             <li><a href="user_functions.html">User functions</a></li>
             <li><a href="user_internal_functions.html">User/Internal functions</a></li>
@@ -122,10 +130,10 @@ Description of the use and inner workings of PHPlot:
 <h3>The Authors</h3>
 <p>
 <ul>
-    <li>Please fill me with your info...</li>
-    <li>Please fill me with your info...</li>
-    <li>Please fill me with your info...</li>
-    <li>Please fill me with your info...</li>
+    <li>Please fill me...</li>
+    <li>Please fill me...</li>
+    <li>Please fill me...</li>
+    <li>Please fill me...</li>
 </p>
 
 <p class="foot">$Id$</p>
