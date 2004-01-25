f951909f0d581149d1b96834f89a93ad888842dd
migueldb <migueldb@users.sf.net>
2004-01-25 17:21:58 +0000

    *** empty log message ***
    git-svn-id: https://svn.code.sf.net/p/phplot/code/trunk@157 1589d9c6-5bb9-42a2-b2f8-3691a6793a8d

--- phplot/examples/example8.php
+++ phplot/examples/example8.php
@@ -9,26 +9,25 @@ $graph->SetDataType("data-data-error");  //Must be called before SetDataValues
 $graph->SetNewPlotAreaPixels(90,40,540,190);
 $graph->SetDataValues($example_data);
 
-$graph->SetXGridLabelType("time");
-$graph->SetXDataLabelAngle(90);
-$graph->SetXLabel("");
-$graph->SetYLabel("Price");
-$graph->SetVertTickIncrement(20);
-$graph->SetHorizTickIncrement(2679000);
+$graph->SetXLabelType("time");
+$graph->SetXLabelAngle(90);
+$graph->SetXTitle("");
+$graph->SetYTitle("Price");
+$graph->SetYTickIncrement(20);
+$graph->SetXTickIncrement(2679000);
 $graph->SetXTimeFormat("%b %y");
 $graph->SetPlotType("lines");
 $graph->SetErrorBarShape("line");
 $graph->SetPointShape("halfline");
 $graph->SetYScaleType("log");
-$graph->SetLineWidth(1);
+$graph->SetLineWidths(array(1));
 $graph->SetPlotAreaWorld(883634400,1,915095000,140);
-
+$graph->SetXDataLabelPos('none');
 $graph->DrawGraph();
-?>
-<?php
+
 //Now do the second chart on the image
+
 unset($example_data);
-$graph->SetPrintImage(1); //Now draw the image 
 
 $graph->SetYScaleType("linear");
 include("./data_date.php");
@@ -39,16 +38,16 @@ $graph->SetDataValues($example_data);
 $graph->SetNewPlotAreaPixels(90,260,540,350);
 $graph->SetDataValues($example_data);
 
-$graph->SetXGridLabelType("time");
-$graph->SetXDataLabelAngle(90);
-$graph->SetXLabel("");
-$graph->SetYLabel("Volume");
-$graph->SetVertTickIncrement(30);
+$graph->SetXLabelType("time");
+$graph->SetXLabelAngle(90);
+$graph->SetXTitle("");
+$graph->SetYTitle("Volume");
+$graph->SetYTickIncrement(30);
 $graph->SetPlotType("thinbarline");
 
 //Set how to display the x-axis ticks
 $graph->SetXTimeFormat("%b %y");
-$graph->SetHorizTickIncrement(2679000);
+$graph->SetXTickIncrement(2679000);
 $graph->SetXAxisPosition(0);  //Have to reset it after log plots
 
 //Set Plot to go from x = Jan 1 1998, to x = Dec 31 1998
