8c9385a1c8c18fb5caeb8d0cfcf22219ea3c0d86
lbayuk <lbayuk@users.sf.net>
2009-06-12 01:46:48 +0000

    Change licensing to LGPL
    git-svn-id: https://svn.code.sf.net/p/phplot/code/trunk@391 1589d9c6-5bb9-42a2-b2f8-3691a6793a8d

--- phplot/phplot.php
+++ phplot/phplot.php
@@ -1,21 +1,34 @@
 <?php
-
 /* $Id$ */
-
 /*
  * PHPLOT Version 5.0.6 + CVS (This is an unreleased CVS version!)
- * Copyright (C) 1998-2009 Afan Ottenheimer.  Released under
- * the GPL and PHP licenses as stated in the the README file which should
- * have been included with this document.
+ *
+ * A PHP class for creating scientific and business charts
+ * Visit http://sourceforge.net/projects/phplot/
+ * for PHPlot documentation, downloads, and discussions.
+ * ---------------------------------------------------------------------
+ * Copyright (C) 1998-2009 Afan Ottenheimer
+ *
+ * This is free software; you can redistribute it and/or
+ * modify it under the terms of the GNU Lesser General Public
+ * License as published by the Free Software Foundation;
+ * version 2.1 of the License.
+ *
+ * This software is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU
+ * Lesser General Public License for more details.
+ *
+ * You should have received a copy of the GNU Lesser General Public
+ * License along with this software; if not, write to the Free Software
+ * Foundation, Inc., 51 Franklin Street, Fifth Floor, Boston, MA 02110-1301 USA
+ * ---------------------------------------------------------------------
  *
  * Co-author and maintainer (2003-2005)
  * Miguel de Benito Delgado <nonick AT vodafone DOT es>
  *
  * Maintainer (2006-present)
  * <lbayuk AT users DOT sourceforge DOT net>
- *
- * Visit http://sourceforge.net/projects/phplot/
- * for PHPlot documentation, downloads, and discussions.
  *
  * Requires PHP 5.2.x or later. (PHP 4 is unsupported as of Jan 2008)
  */
