cf6e052a0a855a3f7a656bfe3f67671a91d1a4e3
lbayuk <lbayuk@users.sf.net>
2009-11-20 00:55:35 +0000

    Use mt_rand not rand for cross-platform consistent results
    git-svn-id: https://svn.code.sf.net/p/phplot/code/trunk@434 1589d9c6-5bb9-42a2-b2f8-3691a6793a8d

--- phplotdocs/examples/thinbarline2.php
+++ phplotdocs/examples/thinbarline2.php
@@ -3,12 +3,12 @@
 require_once 'phplot.php';
 
 # To get repeatable results with 'random' data:
-srand(1);
+mt_srand(1);
 
 # Make some noisy data:
 $data = array();
 for ($i = 0; $i < 100; $i++)
-  $data[] = array('', $i / 4.0 + 2.0 + rand(-20, 20) / 10.0);
+  $data[] = array('', $i / 4.0 + 2.0 + mt_rand(-20, 20) / 10.0);
 
 $plot = new PHPlot(800, 600);
 $plot->SetImageBorderType('plain');
