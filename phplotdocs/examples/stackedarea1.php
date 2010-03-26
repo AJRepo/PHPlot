1d384b3f9d0d92e967dd61f7aeb51ff9912e9557
lbayuk <lbayuk@users.sf.net>
2010-03-26 22:07:06 +0000

    New example for stacked area plot
    git-svn-id: https://svn.code.sf.net/p/phplot/code/trunk@521 1589d9c6-5bb9-42a2-b2f8-3691a6793a8d

---
+++ phplotdocs/examples/stackedarea1.php
@@ -0,0 +1,37 @@
+<?php
+# PHPlot Example: Stacked Area chart
+require_once 'phplot.php';
+
+$data = array(
+  array('1960', 30, 10,  6, 38, 14,  2),
+  array('1970', 20, 17,  9, 32,  2, 20),
+  array('1980', 20, 14, 12, 27,  2, 25),
+  array('1990',  5, 26, 15, 26, 18, 10),
+  array('2000', 28,  0, 18, 16, 33,  5),
+);
+
+$plot = new PHPlot(800, 600);
+$plot->SetImageBorderType('plain');
+
+$plot->SetPlotType('stackedarea');
+$plot->SetDataType('text-data');
+$plot->SetDataValues($data);
+
+# Main plot title:
+$plot->SetTitle('Candy Sales by Flavor');
+
+# Set Y data limits, tick increment, and titles:
+$plot->SetPlotAreaWorld(NULL, 0, NULL, 100);
+$plot->SetYTickIncrement(10);
+$plot->SetYTitle('% of Total');
+$plot->SetXTitle('Year');
+
+# Colors are significant to this data:
+$plot->SetDataColors(array('red', 'green', 'blue', 'yellow', 'cyan', 'magenta'));
+$plot->SetLegend(array('Cherry', 'Lime', 'Lemon', 'Banana', 'Apple', 'Berry'));
+
+# Turn off X tick labels and ticks because they don't apply here:
+$plot->SetXTickLabelPos('none');
+$plot->SetXTickPos('none');
+
+$plot->DrawGraph();
