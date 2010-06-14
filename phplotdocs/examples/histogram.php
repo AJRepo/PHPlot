eb8c8d86c9abc62489c60fcb96ed632350b9d441
lbayuk <lbayuk@users.sf.net>
2010-06-14 02:00:31 +0000

    New: example for truecolor plot
    git-svn-id: https://svn.code.sf.net/p/phplot/code/trunk@575 1589d9c6-5bb9-42a2-b2f8-3691a6793a8d

---
+++ phplotdocs/examples/histogram.php
@@ -0,0 +1,99 @@
+<?php
+# PHPlot Example - Histogram of a Photograph
+# Display a photo image with its value histogram overlaid
+# Note: This requires PHPlot-5.1.1 or higher for Truecolor support.
+require_once 'phplot.php';
+
+# Tunable parameters:
+$param = array(
+    'plot_image_width' => 640,      # Width of final image
+    'plot_image_height' => 480,     # Height of final image
+    'histogram_color' => 'magenta', # Color to use for histogram lines
+    'histogram_alpha' => 50,        # Histogram transparency (0=opaque, 127=clear)
+    'draw_border' => True,          # If true, put a border around the histogram
+    'border_color' => 'red',        # Border color, if draw_border is true
+    'hx' => 0.6,                    # Upper left X relative position of histogram
+    'hy' => 0.0,                    # Upper left Y relative position of histogram
+    'h_width' => 0.4,               # Relative width of histogram
+    'h_height' => 0.35,             # Relative height of histogram
+);
+
+/*
+  Make a histogram from an image file, which can be palette or truecolor.
+  Returns an array $histogram[i] where i is from 0 to 255. Each histogram[i]
+  is the number of pixels in the image with grayscale value i.
+  (Grayscale is computed using the NTSC formula.)
+*/
+function get_histogram($image_file)
+{
+    list($width, $height, $imtype) = getimagesize($image_file);
+    if (!empty($width)) {
+        switch ($imtype) {
+            case IMAGETYPE_JPEG:
+                $im = imagecreatefromjpeg($image_file);
+                break;
+            case IMAGETYPE_PNG:
+                $im = imagecreatefrompng($image_file);
+                break;
+            case IMAGETYPE_GIF:
+                $im = imagecreatefromgif($image_file);
+                break;
+        }
+    }
+    if (empty($width) || empty($im)) {
+        fwrite(STDERR, "Error invalid image file name: $image_file\n");
+        return NULL;
+    }
+
+    # Initialize the histogram counters:
+    $histogram = array_fill(0, 256, 0);
+
+    # Process every pixel. Get the color components and compute the gray value.
+    for ($y = 0; $y < $height; $y++) {
+        for ($x = 0; $x < $width; $x++) {
+            $pix = imagecolorsforindex($im, imagecolorat($im, $x, $y));
+            $value = (int)(0.3 * $pix['red'] + 0.59 * $pix['green'] + 0.11 * $pix['blue']);
+            $histogram[$value]++;
+        }
+    }
+    return $histogram;
+}
+
+/*
+  Make a 'plot', containing a scaled-down version of an image with
+  a histogram overlay.
+*/
+function plot_histogram($image_filename, $param)
+{
+    extract($param);
+    $histo = get_histogram($image_filename);
+    if (empty($histo)) return;
+    for ($i = 0; $i < 256; $i++) $data[$i] = array('', $histo[$i]);
+    $p = new PHPlot_truecolor($plot_image_width, $plot_image_height);
+    $p->SetBgImage($image_filename, 'scale');
+    $p->SetDataType('text-data');
+    $p->SetGridColor('white:127');  # Make the axis lines invisible
+    $p->SetDataValues($data);
+    $p->SetXDataLabelPos('none');
+    $p->SetXTickLabelPos('none');
+    $p->SetYTickLabelPos('none');
+    $p->SetXTickPos('none');
+    $p->SetYTickPos('none');
+    $p->SetDrawYGrid(False);
+    $p->SetDataColors($histogram_color, NULL, $histogram_alpha);
+    $p->SetPlotType('thinbarline');
+    if ($draw_border) {
+        $p->SetGridColor($border_color);
+        $p->SetPlotBorderType('full');
+    }
+    # Compute the position of the histogram plot within the image.
+    $hx0 = (int)($hx * $plot_image_width);
+    $hy0 = (int)($hy * $plot_image_height);
+    $hx1 = (int)($h_width * $plot_image_width) + $hx0;
+    $hy1 = (int)($h_height * $plot_image_height) + $hy0;
+    $p->SetPlotAreaPixels($hx0, $hy0, $hx1, $hy1);
+    $p->DrawGraph();
+}
+
+/* Demo main. */
+plot_histogram('examples/geese.jpg', $param);
