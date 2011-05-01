8ba56448466c3be1aa6c48a37097488009f2fa41
lbayuk <lbayuk@users.sf.net>
2011-05-01 18:10:55 +0000

    New example (2 scripts) for legend with point shapes
    git-svn-id: https://svn.code.sf.net/p/phplot/code/trunk@887 1589d9c6-5bb9-42a2-b2f8-3691a6793a8d

---
+++ phplotdocs/examples/legendshape.php
@@ -0,0 +1,38 @@
+<?php
+# PHPlot Example: Linepoints plot with legend using color boxes or shape markers
+require_once 'phplot.php';
+
+# The variable $use_shapes can be set to TRUE in another
+# script which calls this script, to use shape markers
+# rather than color boxes in the legend.
+if (empty($use_shapes)) $use_shapes = FALSE;
+
+# Use data labels to display only the points we want,
+# but specify the same values for X to get the correct
+# spacing.
+$data = array(
+  array('1990', 1990, 41308, 21015, 62634),
+  array('1995', 1995, 44310, 13883, 61529),
+  array('2000', 2000, 46772,  9000, 59421),
+  array('2004', 2004, 46887,  7738, 57754),
+  array('2006', 2006, 45441,  6888, 53179),
+  array('2008', 2008, 42757,  5840, 49115),
+);
+$legend_text = array('Morning Papers', 'Evening Papers', 'Sunday Papers');
+
+$plot = new PHPlot(800, 600);
+$plot->SetImageBorderType('plain'); // Improves presentation in the manual
+$plot->SetTitle("US Daily Newspaper Circulation\n"
+  . '(Legend using ' . ($use_shapes ? 'shape markers' : 'color boxes') . ')');
+$plot->SetPlotType('linepoints');
+$plot->SetDataType('data-data');
+$plot->SetDataValues($data);
+$plot->SetPlotAreaWorld(1988, 0, 2010, 80000);
+$plot->SetYTickIncrement(10000);
+$plot->SetLegend($legend_text);
+$plot->SetXTickPos('none');
+$plot->SetDrawXDataLabelLines(True);
+$plot->SetLegendUseShapes($use_shapes); // Use color boxes or shapes
+$plot->SetPointSizes(12); // Make points bigger for visibility
+
+$plot->DrawGraph();
---
+++ phplotdocs/examples/legendshape1.php
@@ -0,0 +1,5 @@
+<?php
+# PHPlot Example: Linepoints plot with legend using shape markers
+# See the script named below for details.
+$use_shapes = TRUE;
+require_once 'examples/legendshape.php';
