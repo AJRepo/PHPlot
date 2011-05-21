8bd30e80e47722c51869653e3bc1ac39b55ed99f
lbayuk <lbayuk@users.sf.net>
2011-05-21 23:10:38 +0000

    New example: Y tick anchor, set tick anchor
    git-svn-id: https://svn.code.sf.net/p/phplot/code/trunk@921 1589d9c6-5bb9-42a2-b2f8-3691a6793a8d

---
+++ phplotdocs/examples/ytickanchor1.php
@@ -0,0 +1,5 @@
+<?php
+# PHPlot Example: Using a Y tick anchor to force a tick at 0.
+# This sets a variable and calls the script directly above.
+$set_anchor = 0;
+require_once 'examples/ytickanchor.php';
