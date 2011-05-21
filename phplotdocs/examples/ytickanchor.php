bc317bda6271a8fb777008eb171a2b69965a7c42
lbayuk <lbayuk@users.sf.net>
2011-05-21 23:10:36 +0000

    New example: Y tick anchor, basis
    git-svn-id: https://svn.code.sf.net/p/phplot/code/trunk@920 1589d9c6-5bb9-42a2-b2f8-3691a6793a8d

---
+++ phplotdocs/examples/ytickanchor.php
@@ -0,0 +1,36 @@
+<?php
+# PHPlot Example: Using a Y tick anchor to force a tick at 0.
+# This requires PHPlot >= 5.4.0
+require_once 'phplot.php';
+
+# The variable $set_anchor can be set to a value in another script
+# which calls this script, to set the Y anchor to that value.
+if (isset($set_anchor))
+    $case = "with Y tick anchor at $set_anchor";
+else
+    $case = "without Y tick anchor";
+
+# Function to plot:
+function f($x)
+{
+  if ($x == 0) return 0;
+  return 5 + 8 * sin(200 * M_PI / $x);
+}
+
+$data = array();
+for ($x = 0; $x < 500; $x++)
+  $data[] = array('', $x, f($x));
+
+$plot = new PHPlot(800, 600);
+$plot->SetImageBorderType('plain'); // For presentation in the manual
+$plot->SetTitle("Example $case");
+$plot->SetDataType('data-data');
+$plot->SetDataValues($data);
+$plot->SetPlotType('lines');
+$plot->SetXTickLabelPos('none');
+$plot->SetXTickPos('none');
+
+if (isset($set_anchor))
+    $plot->SetYTickAnchor($set_anchor);
+
+$plot->DrawGraph();
