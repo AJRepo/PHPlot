f17717779cb9660b221a286d4157ebec705d23dc
lbayuk <lbayuk@users.sf.net>
2011-12-30 17:01:48 +0000

    New: PHPlot test - Pie Chart Sizing and Label Variations - Label Type (b)
    git-svn-id: https://svn.code.sf.net/p/phplot/code/trunk@1065 1589d9c6-5bb9-42a2-b2f8-3691a6793a8d

---
+++ phplottest/piesize4b.php
@@ -0,0 +1,13 @@
+<?php
+# $Id$
+# PHPlot test - Pie Chart Sizing and Label Variations - Label Type (b)
+# This is a parameterized test. See the script named at the bottom for details.
+$tp = array(
+  'suffix' => 'Label Formatting',    # Title part 2
+  'data_choice' => 1,          # Select data array: 1 or 2
+  'pie_label_args' => array('label', 'printf', '"%s"'),
+  'ttfonts' => TRUE,           # Use TrueType fonts?
+  'font_size' => 10,           # If set, TrueType or GD font size
+  'precision_y' => NULL,       # Default precision, SetPrecisionY()
+  );
+require 'piesize.php';
