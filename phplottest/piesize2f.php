b23fce3150a92bfa66aad7448504c8b984418382
lbayuk <lbayuk@users.sf.net>
2011-12-30 17:00:52 +0000

    New: PHPlot test - Pie Chart Sizing and Label Variations - label scale position (f)
    git-svn-id: https://svn.code.sf.net/p/phplot/code/trunk@1053 1589d9c6-5bb9-42a2-b2f8-3691a6793a8d

---
+++ phplottest/piesize2f.php
@@ -0,0 +1,9 @@
+<?php
+# $Id$
+# PHPlot test - Pie Chart Sizing and Label Variations - label scale position (f)
+# This is a parameterized test. See the script named at the bottom for details.
+$tp = array(
+  'suffix' => 'labels well outside pie',           # Title part 2
+  'label_pos' => 0.6,             # If set, label position SetLabelScalePosition()
+  );
+require 'piesize.php';
