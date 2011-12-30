fae126acc716e101b6a9ea2a01c0b1879ca8c681
lbayuk <lbayuk@users.sf.net>
2011-12-30 17:03:20 +0000

    New: PHPlot test - Pie Chart Sizing and Label Variations - label margin check (d)
    git-svn-id: https://svn.code.sf.net/p/phplot/code/trunk@1085 1589d9c6-5bb9-42a2-b2f8-3691a6793a8d

---
+++ phplottest/piesize6d.php
@@ -0,0 +1,17 @@
+<?php
+# $Id$
+# PHPlot test - Pie Chart Sizing and Label Variations - label margin check (d)
+# This is a parameterized test. See the script named at the bottom for details.
+$tp = array(
+  'suffix' => 'Label margin check, height constrained, on pie edge',           # Title part 2
+  'data_choice' => 2,          # Select data array: 1 or 2
+  'n_slices' => 72,            # For data_choice==2, number of slices
+  'label_pos' => 0.5,      # If set, label position SetLabelScalePosition()
+  'pie_label_args' => array('index', 'printf', 'E[%02d]E'), # If set array of args to SetPieLabelType()
+  'ttfonts' => TRUE,          # Use TrueType fonts?
+  'font_size' => 14,         # If set, TrueType or GD font size
+  'shading' => 0,           # If set, pie shading SetShading()
+  'plot_border' => 'full',       # If set, plot border type SetPlotBorderType()
+  'plot_margins' => array(10, 10, 100, 100),      # If set, array SetMarginsPixels(l,r,t,b)
+  );
+require 'piesize.php';
