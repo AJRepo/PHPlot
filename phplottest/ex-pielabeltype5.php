4c0bfdac4f3cd6bf6cd775926e55d19cec028ad4
lbayuk <lbayuk@users.sf.net>
2011-12-30 16:58:42 +0000

    New: PHPlot Example: Pie Chart Label Types - Multi-part labels
    git-svn-id: https://svn.code.sf.net/p/phplot/code/trunk@1028 1589d9c6-5bb9-42a2-b2f8-3691a6793a8d

---
+++ phplottest/ex-pielabeltype5.php
@@ -0,0 +1,22 @@
+<?php
+# $Id$
+# PHPlot Example: Pie Chart Label Types - Multi-part labels
+# This requires PHPlot >= 5.6.0
+require_once 'phplot.php';
+require_once 'ex-pielabeltypedata.php'; // Defines $data and $title
+
+function mycallback($str)
+{
+    list($percent, $label) = explode(' ', $str, 2);
+    return sprintf('%s (%.1f%%)', $label, $percent);
+}
+
+$plot = new PHPlot(800, 600);
+$plot->SetImageBorderType('plain'); // Improves presentation in the manual
+$plot->SetPlotType('pie');
+$plot->SetDataType('text-data-single');
+$plot->SetDataValues($data);
+$plot->SetTitle($title);
+# Set label type: combine 2 fields and pass to custom formatting function
+$plot->SetPieLabelType(array('percent', 'label'), 'custom', 'mycallback');
+$plot->DrawGraph();
