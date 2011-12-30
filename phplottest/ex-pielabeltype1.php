2b57a52e62100dcb8fee848c514174df802d7d4d
lbayuk <lbayuk@users.sf.net>
2011-12-30 16:58:19 +0000

    New: PHPlot Example: Pie Chart Label Types - baseline, default label type
    git-svn-id: https://svn.code.sf.net/p/phplot/code/trunk@1024 1589d9c6-5bb9-42a2-b2f8-3691a6793a8d

---
+++ phplottest/ex-pielabeltype1.php
@@ -0,0 +1,14 @@
+<?php
+# $Id$
+# PHPlot Example: Pie Chart Label Types - baseline, default label type
+# This requires PHPlot >= 5.6.0
+require_once 'phplot.php';
+require_once 'ex-pielabeltypedata.php'; // Defines $data and $title
+
+$plot = new PHPlot(800, 600);
+$plot->SetImageBorderType('plain'); // Improves presentation in the manual
+$plot->SetPlotType('pie');
+$plot->SetDataType('text-data-single');
+$plot->SetDataValues($data);
+$plot->SetTitle($title);
+$plot->DrawGraph();
