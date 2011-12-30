88fdc4eb5a7d747528b68c0a78d9ffd7ea6bb61f
lbayuk <lbayuk@users.sf.net>
2011-12-30 17:00:11 +0000

    New: PHPlot test - Pie Chart Sizing and Label Variations - plot border (a)
    git-svn-id: https://svn.code.sf.net/p/phplot/code/trunk@1046 1589d9c6-5bb9-42a2-b2f8-3691a6793a8d

---
+++ phplottest/piesize1a.php
@@ -0,0 +1,9 @@
+<?php
+# $Id$
+# PHPlot test - Pie Chart Sizing and Label Variations - plot border (a)
+# This is a parameterized test. See the script named at the bottom for details.
+$tp = array(
+  'suffix' => 'Border=full',           # Title part 2
+  'plot_border' => 'full',       # If set, plot border type SetPlotBorderType()
+  );
+require 'piesize.php';
