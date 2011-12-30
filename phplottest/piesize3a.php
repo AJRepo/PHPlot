1d60c92a792bd70c3e9394375b948c0019c13cdd
lbayuk <lbayuk@users.sf.net>
2011-12-30 17:00:57 +0000

    New: PHPlot test - Pie Chart Sizing and Label Variations - Pie/image size (a)
    git-svn-id: https://svn.code.sf.net/p/phplot/code/trunk@1054 1589d9c6-5bb9-42a2-b2f8-3691a6793a8d

---
+++ phplottest/piesize3a.php
@@ -0,0 +1,9 @@
+<?php
+# $Id$
+# PHPlot test - Pie Chart Sizing and Label Variations - Pie/image size (a)
+# This is a parameterized test. See the script named at the bottom for details.
+$tp = array(
+  'suffix' => 'Landscape',     # Title part 2
+  'image_aspect' => 'L',       # Image aspect: S=square, P=portrait, L=landscape
+  );
+require 'piesize.php';
