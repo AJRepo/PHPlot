2ed9dc66f40d79194cd499c8017b5e5c9faa5a03
lbayuk <lbayuk@users.sf.net>
2011-12-30 16:59:05 +0000

    New: PHPlot test - Pie Chart Sizing - auto vs manual - case 2
    git-svn-id: https://svn.code.sf.net/p/phplot/code/trunk@1033 1589d9c6-5bb9-42a2-b2f8-3691a6793a8d

---
+++ phplottest/pieautosize2.php
@@ -0,0 +1,7 @@
+<?php
+# $Id$
+# PHPlot test - Pie Chart Sizing - auto vs manual - case 2
+# This is a parameterized test. See the script named at the bottom for details.
+$pie_autosize = TRUE;
+$label_pos = 0.4;
+require_once 'pieautosize.php';
