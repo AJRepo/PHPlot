ff36d5c092db538946ac59f603e1abedc8139b67
lbayuk <lbayuk@users.sf.net>
2011-12-30 16:59:51 +0000

    New: PHPlot test - Pie Chart minimum size cut-off - case 4
    git-svn-id: https://svn.code.sf.net/p/phplot/code/trunk@1043 1589d9c6-5bb9-42a2-b2f8-3691a6793a8d

---
+++ phplottest/pieminsize4.php
@@ -0,0 +1,8 @@
+<?php
+# $Id$
+# PHPlot test - Pie Chart minimum size cut-off - case 4
+# This is a parameterized test. See the script named at the bottom for details.
+$label_size = 5;
+$min_size_factor = 0.80;
+$suffix = 'Increase min_size, Labels Fit';
+require_once 'pieminsize.php';
