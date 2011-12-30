2f66050ec9989f2b5547b157d55306672ee8cd0c
lbayuk <lbayuk@users.sf.net>
2011-12-30 16:58:32 +0000

    New: PHPlot Example: Pie Chart Label Types - Formatted values as labels
    git-svn-id: https://svn.code.sf.net/p/phplot/code/trunk@1026 1589d9c6-5bb9-42a2-b2f8-3691a6793a8d

---
+++ phplottest/ex-pielabeltype3.php
@@ -0,0 +1,16 @@
+<?php
+# $Id$
+# PHPlot Example: Pie Chart Label Types - Formatted values as labels
+# This requires PHPlot >= 5.6.0
+require_once 'phplot.php';
+require_once 'ex-pielabeltypedata.php'; // Defines $data and $title
+
+$plot = new PHPlot(800, 600);
+$plot->SetImageBorderType('plain'); // Improves presentation in the manual
+$plot->SetPlotType('pie');
+$plot->SetDataType('text-data-single');
+$plot->SetDataValues($data);
+$plot->SetTitle($title);
+# Set label type: segment values, formatted with 2 decimal places
+$plot->SetPieLabelType('value', 'data', 2);
+$plot->DrawGraph();
