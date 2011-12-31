b3b84b3f20427aa5c2aaa12a1e6aa3523a8b9439
lbayuk <lbayuk@users.sf.net>
2011-12-31 03:36:44 +0000

    New: Example for SetPieLabelType
    git-svn-id: https://svn.code.sf.net/p/phplot/code/trunk@1108 1589d9c6-5bb9-42a2-b2f8-3691a6793a8d

---
+++ phplotdocs/examples/pielabeltype4.php
@@ -0,0 +1,22 @@
+<?php
+# PHPlot Example: Pie Chart Label Types - Index and custom callback
+# This requires PHPlot >= 5.6.0
+require_once 'phplot.php';
+require_once 'pielabeltypedata.php'; // Defines $data and $title
+
+$mylabels = array('A', 'B', 'C', 'D', 'E', 'F', 'G', 'H', 'I', 'J');
+function mycallback($index)
+{
+    global $mylabels;
+    return $mylabels[$index];
+}
+
+$plot = new PHPlot(800, 600);
+$plot->SetImageBorderType('plain'); // Improves presentation in the manual
+$plot->SetPlotType('pie');
+$plot->SetDataType('text-data-single');
+$plot->SetDataValues($data);
+$plot->SetTitle($title);
+# Set label type: Pass segment index (0-N) to custom formating function
+$plot->SetPieLabelType('index', 'custom', 'mycallback');
+$plot->DrawGraph();
---
+++ phplotdocs/examples/pielabeltypedata.php
@@ -0,0 +1,16 @@
+<?php
+# PHPlot Example: Pie Chart Label Types - Data array
+# This is used by several examples
+$title = 'Energy Production By Source, 2005';
+$data = array(
+    array('Biomass',        3120.43),
+    array('Coal',          23185.20), 
+    array('Geotherm.',       343.74),
+    array('Hydro',          2703.92),
+    array('NGPL',           2334.04),
+    array('Nat. Gas',      18574.55), 
+    array('Nuclear',        8160.49),
+    array('Oil',           10963.63),
+    array('Solar',            66.17),
+    array('Wind',            178.81),
+);
