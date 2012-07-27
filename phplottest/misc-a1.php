e8c5cbfbdc5ba1a22646a97874acd4771b1c5e7c
lbayuk <lbayuk@users.sf.net>
2012-07-27 02:17:04 +0000

    Remove incorrect comment and unneeded color definitions
    git-svn-id: https://svn.code.sf.net/p/phplot/code/trunk@1352 1589d9c6-5bb9-42a2-b2f8-3691a6793a8d

--- phplottest/misc-a1.php
+++ phplottest/misc-a1.php
@@ -3,21 +3,11 @@
 # PHPlot test: Miscellaneous options: color map, line spacing, grid line style
 # This is a parameterized test. See the script named at the bottom for details.
 
-# When defining a color map, you either need to also define the data colors,
-# or assign the same color names to new values.
-# This test originally redefined the existing data colors to have new
-# values, but that broke when the default data colors changed.
-# So now we do both a color map and a data colors list.
-# (Which is what the manual always said you need.)
-
 # Define 4 data color names for the 4 data sets:
 $dcolors = array('data1', 'data2', 'data3', 'data4');
 
-# Define a color map. Needs black, gray, white, plus our data colors.
+# Define a color map:
 $graymap = array(
-  'black'   => array(0x00, 0x00, 0x00),
-  'white'   => array(0xff, 0xff, 0xff),
-  'gray'    => array(0x99, 0x99, 0x99),
   'data1'   => array(0xcc, 0x66, 0xff),
   'data2'   => array(0x33, 0xcc, 0xff),
   'data3'   => array(0x66, 0xff, 0x99),
