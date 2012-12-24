eccf2d46724e58a96e2d48c2c65270b72004817f
lbayuk <lbayuk@users.sf.net>
2012-12-24 17:08:41 +0000

    Change test plot type due to new PHPlot feature
    git-svn-id: https://svn.code.sf.net/p/phplot/code/trunk@1460 1589d9c6-5bb9-42a2-b2f8-3691a6793a8d

--- phplottest/legshape2.php
+++ phplottest/legshape2.php
@@ -3,9 +3,9 @@
 # Legend shape marker tests - case 2
 # This is a parameterized test. See the script named at the bottom for details.
 $tp = array(
-  'suffix' => 'Non-points plot, revert to colorboxes, TTF',  # Title line 2
+  'suffix' => 'Plot type does not support shape markers, reverts to colorboxes, TTF',  # Title line 2
   'useshapes' => True,     # True for shape markers, false for color boxes
   'fontsize' => 14,       # Use TT font at this size
-  'plottype' => 'lines',   # Plot type, points or linepoints
+  'plottype' => 'thinbarline',   # Plot type, points or linepoints
   );
 require 'legshape.php';
