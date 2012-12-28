80e38a273aef2798bbaaefaadc08811534d65130
lbayuk <lbayuk@users.sf.net>
2012-12-28 03:33:00 +0000

    Remove check for CVS, add check for new version_id const
    git-svn-id: https://svn.code.sf.net/p/phplot/code/trunk@1472 1589d9c6-5bb9-42a2-b2f8-3691a6793a8d

--- phplottest/testversion.php
+++ phplottest/testversion.php
@@ -1,6 +1,6 @@
 <?php
 # $Id$
-# Testing phplot - Version constant (PHPlot>=5.4.0)
+# Testing phplot - Version constants (PHPlot>=5.4.0)
 # This is a unit test. No image is produced.
 require_once 'phplot.php';
 $failure = '';
@@ -10,7 +10,7 @@ if (!defined('PHPlot::version')) {
     $failure .= "phplot does not define a version constant\n";
 
 # Check: Format #.#.#...
-} elseif (!preg_match('/^(\\d+\\.\\d+\\.\\d)(.*)/', PHPlot::version, $v)) {
+} elseif (!preg_match('/^(\\d+\\.\\d+\\.\\d+)/', PHPlot::version, $v)) {
     $failure .= "phplot version constant is not in the expected format\n";
 } else {
     # These checks are only if the version constant looks right.
@@ -20,37 +20,40 @@ if (!defined('PHPlot::version')) {
     # Open the source file, using the INCLUDE path.
     $f = fopen('phplot.php', 'r', TRUE);
     if (!$f) exit(1); // Error already reported by fopen. Fatal.
-    $found = '';
+    $found = False;
     for ($i = 0; $i < 10; $i++) {
         $s = fgets($f);
         if ($s === False) break;
-        if (preg_match('/PHPLOT Version/i', $s)) {
-            $found = $s;
+        if (preg_match('/PHPLOT Version +(\\d+\\.\\d+\\.\\d+)/i', $s, $m)) {
+            $found = True;
             break;
         }
     }
     fclose($f);
     if (!$found) {
         $failure .= "Unable to read version comment from source file\n";
-    } else {
-        if (!preg_match('/PHPLOT Version (\\d+\\.\\d+\\.\\d+)(.*)/',
-                        $found, $m)) {
-            $failure .= "Unable to match version comment in source file\n";
-        } else {
-            if ($m[1] != $v[1]) {
-                $failure .= "File comment version {$m[1]} != version constant {$v[1]}\n";
-            }
-            # Check: both say CVS or neither says CVS:
-            if (preg_match('/CVS/', $m[2]) != preg_match('/CVS/', $v[2])) {
-                $failure .= "Disagree on CVS status:\n"
-                          . "    File comment: {$m[2]}\n"
-                          . "Version constant: {$v[2]}\n";
-            }
-        }
+    } elseif ($m[1] != $v[1]) {
+        $failure .= "File comment version {$m[1]} != version constant {$v[1]}\n";
     }
 }
+
+# This was added in 6.0.0, so don't fail the test if it is missing.
+if (defined('PHPlot::version_id')) {
+    $v_s = ((int)(PHPlot::version_id / 10000)) . '.' .
+           ((int)(PHPlot::version_id / 100) % 100) . '.' . 
+           (int)(PHPlot::version_id % 100);
+
+    if ($v_s != $v[1]) {
+        $failure .= "Mismatch between version: " . PHPlot::version . "\n"
+                  . "          and version_id: " . PHPlot::version_id . "\n";
+    }
+} else {
+    echo "Note: PHPlot version_id constant is not defined\n";
+}
+
 if (!empty($failure)) {
     fwrite(STDERR, "Version test failed:\n$failure\n");
     exit(1); // Test framework says: exit on error, fall off if pass.
 }
+
 echo "Pass: PHPlot version = " . PHPlot::version . "\n";
