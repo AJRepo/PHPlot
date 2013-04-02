409042bf4fc8ab5ddce9d49f79b3cb555982512f
lbayuk <lbayuk@users.sf.net>
2013-04-02 22:08:50 +0000

    Set minticks to (old default) 5 so the tests work
    git-svn-id: https://svn.code.sf.net/p/phplot/code/trunk@1573 1589d9c6-5bb9-42a2-b2f8-3691a6793a8d

--- phplottest/range-auto7dec4e.php
+++ phplottest/range-auto7dec4e.php
@@ -8,6 +8,7 @@ $tp = array(
   'max' => 25000,          # Highest data value
   'adjust_mode' => 'T',    # Range adjust mode: 'T', 'R', or 'I'
   'adjust_amount' => 0,    #  % of range for adjustment
+  'mintick' => 5,          # Minimum tick count
   'tick_mode' => 'decimal', # Tick selection mode
   );
 require 'range-auto.php';
