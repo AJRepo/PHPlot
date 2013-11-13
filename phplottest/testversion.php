2324cea8fe57685910371b080a8da946f4cad825
lbayuk <lbayuk@users.sf.net>
2013-11-13 22:09:57 +0000

    Accept phpdoc version tag as alternative to version comment
    git-svn-id: https://svn.code.sf.net/p/phplot/code/trunk@1649 1589d9c6-5bb9-42a2-b2f8-3691a6793a8d

--- phplottest/testversion.php
+++ phplottest/testversion.php
@@ -13,10 +13,18 @@ if (!defined('PHPlot::version')) {
 } elseif (!preg_match('/^(\\d+\\.\\d+\\.\\d+)/', PHPlot::version, $v)) {
     $failure .= "phplot version constant is not in the expected format\n";
 } else {
+    $version_from_constant = $v[1];
+
     # These checks are only if the version constant looks right.
 
-    # Read the top ~10 lines of the source, looking for the title.
-    # This needs to somewhat match the version constant.
+    # Read the top ~25 lines of the source, looking for the version comment.
+    # This could be a comment in the source:  "PHPLOT Version x.y.z"
+    # Or a phpdoc docblock-type tag:          "@version x.y.z"
+    #   Whichever is found first is used. Source code should contain only
+    #   one of these.
+    # The version comment or tag, if found,  is checked against the value of
+    # the version constant.
+    #
     # Open the source file, using the INCLUDE path.
     $f = fopen('phplot.php', 'r', TRUE);
     if (!$f) exit(1); // Error already reported by fopen. Fatal.
@@ -24,7 +32,8 @@ if (!defined('PHPlot::version')) {
     for ($i = 0; $i < 10; $i++) {
         $s = fgets($f);
         if ($s === False) break;
-        if (preg_match('/PHPLOT Version +(\\d+\\.\\d+\\.\\d+)/i', $s, $m)) {
+        if (preg_match('/(PHPLOT Version|@version) +(\\d+\\.\\d+\\.\\d+)/i', $s, $m)) {
+            $version_from_comment = $m[2];
             $found = True;
             break;
         }
@@ -32,18 +41,19 @@ if (!defined('PHPlot::version')) {
     fclose($f);
     if (!$found) {
         $failure .= "Unable to read version comment from source file\n";
-    } elseif ($m[1] != $v[1]) {
-        $failure .= "File comment version {$m[1]} != version constant {$v[1]}\n";
+    } elseif ($version_from_constant != $version_from_comment) {
+        $failure .= "File comment version $version_from_comment"
+                  . " != version constant $version_from_constant\n";
     }
 }
 
 # This was added in 6.0.0, so don't fail the test if it is missing.
 if (defined('PHPlot::version_id')) {
-    $v_s = ((int)(PHPlot::version_id / 10000)) . '.' .
+    $version_from_id = ((int)(PHPlot::version_id / 10000)) . '.' .
            ((int)(PHPlot::version_id / 100) % 100) . '.' . 
            (int)(PHPlot::version_id % 100);
 
-    if ($v_s != $v[1]) {
+    if ($version_from_id != $version_from_constant) {
         $failure .= "Mismatch between version: " . PHPlot::version . "\n"
                   . "          and version_id: " . PHPlot::version_id . "\n";
     }
