d3c4df3227d3101b56b1986a9ecbb0d25d1d2177
lbayuk <lbayuk@users.sf.net>
2015-08-30 00:59:11 +0000

    Fix bug #172 for PHP7: Use __construct for constructor name
    git-svn-id: https://svn.code.sf.net/p/phplot/code/trunk@1688 1589d9c6-5bb9-42a2-b2f8-3691a6793a8d

--- phplot/phplot.php
+++ phplot/phplot.php
@@ -644,7 +644,7 @@ class PHPlot
      * @param string $output_file  Path for output file. Omit, or NULL, or '' to mean no output file
      * @param string $input_file   Path to a file to be used as background. Omit, NULL, or '' for none
      */
-    function PHPlot($width=600, $height=400, $output_file=NULL, $input_file=NULL)
+    function __construct($width=600, $height=400, $output_file=NULL, $input_file=NULL)
     {
         $this->initialize('imagecreate', $width, $height, $output_file, $input_file);
     }
