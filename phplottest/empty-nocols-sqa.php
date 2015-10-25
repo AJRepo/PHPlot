9c9e1d2432d56a25313cbe97e6afd5d69e270d27
lbayuk <lbayuk@users.sf.net>
2015-10-25 18:09:13 +0000

    New cases for new plot types
    git-svn-id: https://svn.code.sf.net/p/phplot/code/trunk@1730 1589d9c6-5bb9-42a2-b2f8-3691a6793a8d

---
+++ phplottest/empty-nocols-sqa.php
@@ -0,0 +1,7 @@
+<?php
+# $Id$
+# PHPlot test - empty plot with no Y values at all, squaredarea plot
+# This is a parameterized test. See the script named at the bottom for details.
+$plot_type = 'squaredarea';
+$data_type = 'text-data';
+require 'empty-nocols.php';
