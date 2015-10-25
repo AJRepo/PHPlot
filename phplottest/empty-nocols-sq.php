4310d15085a177f055c646f736ab594542adae1d
lbayuk <lbayuk@users.sf.net>
2015-10-25 18:09:10 +0000

    Fix comment typo
    git-svn-id: https://svn.code.sf.net/p/phplot/code/trunk@1729 1589d9c6-5bb9-42a2-b2f8-3691a6793a8d

--- phplottest/empty-nocols-sq.php
+++ phplottest/empty-nocols-sq.php
@@ -1,6 +1,6 @@
 <?php
 # $Id$
-# PHPlot test - empty plot with no Y values at all, points plot
+# PHPlot test - empty plot with no Y values at all, squared plot
 # This is a parameterized test. See the script named at the bottom for details.
 $plot_type = 'squared';
 $data_type = 'text-data';
